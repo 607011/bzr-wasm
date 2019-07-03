b7e1fa5a36d7b6c1b1bb817a75c5ff899ba7326a
Oliver Lau <ola@ct.de>
2019-07-03 14:51:35 +0200

    WASM code optimized with -O3.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 em++ \
-  -O2 \
+  -O3 \
   -s WASM=1 \
   -s ONLY_MY_CODE=1 \
   -s EXPORTED_FUNCTIONS="['_BZR_init', '_BZR_iterate', '_BZR_pour', '_BZR_rgb_ref', '_BZR_convertToRGB']" \
