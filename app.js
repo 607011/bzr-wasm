e3306df572c9bfff1ef909381dca079a8b75e2e2
Oliver Lau <ola@ct.de>
2019-07-03 13:11:59 +0200

    Renamed variables for localStorage.

--- app.js
+++ app.js
@@ -18,15 +18,15 @@
 
   const DEFAULT_K1 = 3;
   const DEFAULT_K2 = 1;
-  const DEFAULT_G = 14;
-  const DEFAULT_N = 125;
+  const DEFAULT_G = 23;
+  const DEFAULT_N = 132;
   const memory = new WebAssembly.Memory({ initial: 256, maximum: 256 });
   let exports = null;
   let canvas = null;
-  let k1 = localStorage.getItem('k1') || DEFAULT_K1;
-  let k2 = localStorage.getItem('k2') || DEFAULT_K2;
-  let g = localStorage.getItem('g') || DEFAULT_G;
-  let n = localStorage.getItem('n') || DEFAULT_N;
+  let k1 = localStorage.getItem('bzr.k1') || DEFAULT_K1;
+  let k2 = localStorage.getItem('bzr.k2') || DEFAULT_K2;
+  let g = localStorage.getItem('bzr.g') || DEFAULT_G;
+  let n = localStorage.getItem('bzr.n') || DEFAULT_N;
   let t0 = 0;
 
   const WIDTH = 512;
@@ -89,22 +89,22 @@
     fpsEl = document.getElementById('fps');
     document.getElementById('k1').addEventListener('change', event => {
       k1 = parseInt(event.target.value);
-      localStorage.setItem('k1', k1);
+      localStorage.setItem('bzr.k1', k1);
       document.getElementById('k1-value').innerText = k1;
     });
     document.getElementById('k2').addEventListener('change', event => {
       k2 = parseInt(event.target.value);
-      localStorage.setItem('k2', k2);
+      localStorage.setItem('bzr.k2', k2);
       document.getElementById('k2-value').innerText = k2;
     });
     document.getElementById('g').addEventListener('change', event => {
       g = parseInt(event.target.value);
-      localStorage.setItem('g', g);
+      localStorage.setItem('bzr.g', g);
       document.getElementById('g-value').innerText = g;
     });
     document.getElementById('n').addEventListener('change', event => {
       n = parseInt(event.target.value);
-      localStorage.setItem('n', n);
+      localStorage.setItem('bzr.n', n);
       document.getElementById('n-value').innerText = n;
     });
     document.getElementById('k1').value = k1;
